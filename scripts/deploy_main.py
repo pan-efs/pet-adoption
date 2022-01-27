502c78683443e25f361d7663186bd75a633dbf64
pan-efs <pefstrat@gmail.com>
2022-01-27 14:42:05 +0100

    Reformatted

--- scripts/deploy_main.py
+++ scripts/deploy_main.py
@@ -1,5 +1,7 @@
 import os
+
 from scripts.deploy_dog_adoption import *
+
 
 load_dotenv()
 
@@ -9,6 +11,5 @@ def main():
     add_adopter(os.getenv('PUBLIC_KEY'), 25, "male")
     send_donation(0.05)
     add_pet(5, "smurf", 2, "male", "pitbull", 2)
-    adopt_pet(5)
-    #get_balance()
+    #adopt_pet(5)
     terminate_adoption_campaign()
